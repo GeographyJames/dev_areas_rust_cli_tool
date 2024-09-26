7b112fc0c1a7707badf8ae53b60443f7f3cfdcb7
James Campbell <james.e.campbell@outlook.com>
2024-09-26 17:54:23 +0100

    added gdal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use gdal::Dataset;
+use std::path::PathBuf;
+
+use clap::Parser;
+
 fn main() {
-    let _unused_variable = 10;
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+    let _ds = Dataset::open(args.path).unwrap();
+}
+
+#[derive(Parser)]
+struct CliArgs {
+    path: PathBuf,
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,15 @@
+use assert_cmd::Command;
+use predicates::prelude::predicate;
+
 #[test]
-fn firt_test() {
-    panic!()
+fn fails_if_no_arguments_given() {
+    let mut cmd = Command::cargo_bin("dev_areas").unwrap();
+    cmd.assert().failure();
+}
+
+#[test]
+fn fails_if_argument_is_not_valid_file() {
+    let mut cmd = Command::cargo_bin("dev_areas").unwrap();
+    cmd.arg("a_file");
+    cmd.assert().failure();
 }
